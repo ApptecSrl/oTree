ddb5a44130ab7e250b745e573f3f5cbfbbd65313
valoriv <valoriv@yahoo.com>
2015-10-16 16:00:31 +0200

    change dictator app result to save in session all results

--- dictator/views.py
+++ dictator/views.py
@@ -3,6 +3,7 @@ from __future__ import division
 from . import models
 from ._builtin import Page, WaitPage
 from .models import Constants
+from django.utils.translation import ugettext as _
 
 
 def vars_for_all_templates(self):
@@ -63,13 +64,44 @@ class ResultsWaitPage(WaitPage):
 
 
 class Results(Page):
+    template_name = 'global/ResultsTable.html'
 
     def offer(self):
         return Constants.allocated_amount - self.group.kept
 
     def vars_for_template(self):
+
+        payment_result = {
+            'offer':Constants.allocated_amount - self.group.kept,
+            'kept':self.group.kept,
+            'payoff': self.player.payoff
+        }
+
+        if self.player.id_in_group == 1:
+            result_table = [(_('You decided to keep'), payment_result['kept'])]
+        else:
+            result_table = [
+                (_('Participant 1 decided to keep'), payment_result['kept']),
+                (_('You got'), payment_result['offer']),
+            ]
+
+        result_table.append((_('Your total payoff is'), payment_result['payoff']))
+
+        game_results = {
+            'label': 'Dictator game',
+            'results': payment_result,
+            'table': result_table,
+        }
+
+        # store dictator payment result in player session
+        if 'applications_results' in self.player.participant.vars.keys():
+            if game_results['label'] not in [app['label'] for app in self.player.participant.vars['applications_results']]:
+                self.player.participant.vars['applications_results'].append(game_results)
+        else:
+            self.player.participant.vars['applications_results'] = [game_results]
+
         return {
-            'offer': Constants.allocated_amount - self.group.kept,
+            'table': [('Gioco concluso, i risultati verranno mostrati in seguito',)]
         }
 
 
