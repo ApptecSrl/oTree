2e99a17765a76778e25943e1ceecd6c6639aceda
Federico Della Rovere <dellarovere.federico@gmail.com>
2015-10-29 12:08:42 +0100

    edit public_goods result to use with payment_recap app

--- public_goods/views.py
+++ public_goods/views.py
@@ -52,9 +52,43 @@ class Results(Page):
 
     def vars_for_template(self):
 
-        return {
+        payment_result = {
             'total_earnings': self.group.total_contribution * Constants.efficiency_factor,
             'individual_earnings': self.player.payoff - Constants.base_points,
+        }
+
+        result_table = [
+            (_('You contributed'), self.player.contribution),
+            (_('Other participants contributed'),),
+        ]
+        for p in self.player.get_others_in_group():
+            result_table.append(('', p.contribution))
+
+        result_table.append((_('Total contribution'), self.group.total_contribution))
+        result_table.append(('', ''))
+        result_table.append((_('Total earnings from the project'), payment_result['total_earnings']))
+        result_table.append((_('Your earnings from the project'), self.group.individual_share))
+        result_table.append(('', ''))
+        result_table.append((_('Thus in total you earned'), payment_result['individual_earnings']))
+        result_table.append((_('In addition you get a participation fee of'), Constants.base_points))
+        result_table.append(('', ''))
+        result_table.append((_('So in sum you will get'), self.player.payoff))
+
+        game_results = {
+            'label': _('Public goods game'),
+            'results': payment_result,
+            'table': result_table,
+        }
+
+        # store public_goods payment result in player session
+        if 'applications_results' in self.player.participant.vars.keys():
+            if game_results['label'] not in [app['label'] for app in self.player.participant.vars['applications_results']]:
+                self.player.participant.vars['applications_results'].append(game_results)
+        else:
+            self.player.participant.vars['applications_results'] = [game_results]
+
+        return {
+            'table': [(_('Gioco concluso, i risultati verranno mostrati in seguito'),)]
         }
 
 page_sequence = [
