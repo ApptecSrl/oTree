08d92e3765dd9ed49528a018e933fa581530c016
dome.colucci@gmail.com <dome.colucci@gmail.com>
2015-12-04 10:27:51 +0100

    tolto erroneo riferimento a payment info (che invece deve essere payment_recap)

--- settings.py
+++ settings.py
@@ -143,7 +143,7 @@ SESSION_CONFIGS = [
          'name': 'public_goods',
          'display_name': "Public Goods",
          'num_demo_participants': 3,
-         'app_sequence': ['public_goods', 'payment_info'],
+         'app_sequence': ['public_goods', 'payment_recap'],
     },
     {
         'name': 'bare_market',
